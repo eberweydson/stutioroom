7cbf065a334d8c72a4414228b8b6b87eb1254605
eberweydson <eberweydson@gmail.com>
2023-05-30 23:18:08 -0300

    feat: Adicionado tema claro

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { ThemeProvider } from '@mui/material';
+import { BrowserRouter } from "react-router-dom";
+
+import { AppRoutes } from './routes';
+import { LightTheme } from './shared/themes';
 
 export const App = () => {
   return (
-    <div className="App">
-        Inicializando a Aplicação
-    </div>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export { };
+import { Button } from "@mui/material";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => { 
+
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<Button variant="contained" color="primary">Página Inicial</Button>} />
+      <Route path="*" element={<Navigate to ="/pagina-inicial" />} />
+    </Routes>
+  )
+};
